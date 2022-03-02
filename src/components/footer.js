7949ac0aadec9dea20599855835ad0816e7693e7
WaveDashShine <erikcui9020@gmail.com>
2022-03-01 22:56:03 -0800

    move files around and commit stubs

---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+// import css / images up here
+
+function Footer() {
+    return (
+        <div className="I'm the new Footer hey!"></div>
+    );
+};
+
+export default Footer; 
---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+// import css / images up here
+
+function Header() {
+    return (
+        <div className="I'm the new header hey!"></div>
+    );
+};
+
+export default Header; 
